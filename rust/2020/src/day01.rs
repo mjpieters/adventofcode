a8330e25f5cdee78923e8ef4f0cb4be40a052241
Martijn Pieters <mj@zopatista.com>
2020-12-01 15:42:59 +0000

    A BTreeSet is faster, and we only need unsigned integers.

--- rust/2020/src/day01.rs
+++ rust/2020/src/day01.rs
@@ -1,13 +1,13 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 use std::num::ParseIntError;
 
 #[aoc_generator(day1)]
-fn read_coins(input: &str) -> Result<HashSet<i32>, ParseIntError> {
+fn read_coins(input: &str) -> Result<BTreeSet<u32>, ParseIntError> {
     input.lines().map(|l| l.parse()).collect()
 }
 
-fn find_sum(coins: &HashSet<i32>, target: i32) -> Option<(i32, i32)> {
+fn find_sum(coins: &BTreeSet<u32>, target: u32) -> Option<(u32, u32)> {
     for first in coins {
         let second = target.wrapping_sub(*first);
         if coins.contains(&second) {
@@ -18,7 +18,7 @@ fn find_sum(coins: &HashSet<i32>, target: i32) -> Option<(i32, i32)> {
 }
 
 #[aoc(day1, part1)]
-fn part1(coins: &HashSet<i32>) -> i32 {
+fn part1(coins: &BTreeSet<u32>) -> u32 {
     if let Some((first, second)) = find_sum(coins, 2020) {
         return first * second;
     }
@@ -26,9 +26,9 @@ fn part1(coins: &HashSet<i32>) -> i32 {
 }
 
 #[aoc(day1, part2)]
-fn part2(coins: &HashSet<i32>) -> i32 {
+fn part2(coins: &BTreeSet<u32>) -> u32 {
     for selected in coins {
-        if let Some((first, second)) = find_sum(coins, 2020_i32.wrapping_sub(*selected)) {
+        if let Some((first, second)) = find_sum(coins, 2020_u32.wrapping_sub(*selected)) {
             return selected * first * second;
         }
     }
@@ -41,12 +41,12 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        let example: HashSet<i32> = vec![1721, 979, 366, 299, 675, 1456].into_iter().collect();
+        let example: BTreeSet<u32> = vec![1721, 979, 366, 299, 675, 1456].into_iter().collect();
         assert_eq!(part1(&example), 514579);
     }
     #[test]
     fn part2_example() {
-        let example: HashSet<i32> = vec![1721, 979, 366, 299, 675, 1456].into_iter().collect();
+        let example: BTreeSet<u32> = vec![1721, 979, 366, 299, 675, 1456].into_iter().collect();
         assert_eq!(part2(&example), 241861950);
     }
 }
